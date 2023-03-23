c76aef44567427ecb3649b4b19f4fe17ee299322
truong <damlamtruong@gmail.com>
2023-03-23 09:26:19 +0700

    create protobuf and rpc server

---
+++ calculator/server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/lamtruong9x/grpc_project/calculator/proto"
+	"google.golang.org/grpc"
+)
+
+const addr string = "localhost:50123"
+
+type Calculator struct {
+	pb.SumServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Listening on port %v", addr)
+	s := grpc.NewServer()
+	pb.RegisterSumServiceServer(s, &Calculator{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatal(err)
+	}
+}
